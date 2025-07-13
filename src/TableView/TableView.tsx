f584a160aacaa1bf1495db05bde250b7729e3daf
Kusya <nika_nikusya@mail.ru>
2025-07-13 18:22:52 +0300

    feat: add table view component and fetch data from api into it

--- src/TableView/TableView.tsx
+++ src/TableView/TableView.tsx
@@ -1,9 +1,54 @@
 import { Component } from 'react';
 
+export const fetchData = async () => {
+  const response = await fetch(
+    'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+  );
+  if (!response.ok) throw new Error('Error loaging data');
+  return await response.json();
+};
+
 export default class TableView extends Component {
-  //props: Readonly<{}>;
+  state = {
+    data: Array,
+    loading: true,
+    error: null,
+  };
+
+  async componentDidMount() {
+    try {
+      const data = await fetchData();
+      this.setState({ data: data.results, loading: false });
+    } catch (err: Error) {
+      this.setState({ error: err.message, loading: false });
+    }
+  }
+
   render() {
-    //this.props.name
-    return <>`There·should·be·the·results·table·with·a·loader`</>;
+    const { data, loading, error } = this.state;
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+    if (data == null || data.length <= 0) return <div>No items found</div>;
+
+    return (
+      <div>
+        <h3>Pokemons</h3>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Url</th>
+          </tr>
+          {data.map((item) => (
+            <tr key={item.name}>
+              <td>{item.name}</td>
+              <td>
+                <a href={item.url}>href</a>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
   }
 }
