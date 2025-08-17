d1f376eda6ae29f3622f104f6f8b6858b0979e70
Kusya <nika_nikusya@mail.ru>
2025-08-17 06:16:16 +0300

    fix: tailwind fix to work with next

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
     <div>
-      <h1>{t('title')}</h1>
+      <h1 className="px-8">{t('title')}</h1>
       <PokemonSearchPage />
     </div>
   );
--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -6,14 +6,14 @@ export default function MenuLayout() {
   const t = useTranslations('HomePage');
 
   return (
-    <nav className="flex justify-end p-1">
-      <Link href="/about" className="px-4">
+    <nav className="flex justify-end px-2">
+      <Link href="/about" className="px-2">
         {t('about')}
       </Link>
-      <Link href="/" locale="ru">
+      <Link href="/" locale="ru" className="px-2">
         Ru
       </Link>
-      <Link href="/" locale="en">
+      <Link href="/" locale="en" className="px-2">
         En
       </Link>
     </nav>
