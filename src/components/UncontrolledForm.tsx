e4a18bab1d119619bbb4f76df9a04cb0ca3c1d12
Kusya <nika_nikusya@mail.ru>
2025-08-21 19:22:46 +0300

    feat: add fields in uncontrolled form

--- src/components/UncontrolledForm.tsx
+++ src/components/UncontrolledForm.tsx
@@ -3,6 +3,12 @@ import CustomInput from './CustomInput';
 
 export default function UncontrolledForm() {
   const nameInputRef = useRef<HTMLInputElement | null>(null);
+  const ageInputRef = useRef<HTMLInputElement | null>(null);
+  const emailInputRef = useRef<HTMLInputElement | null>(null);
+  const pass1InputRef = useRef<HTMLInputElement | null>(null);
+  const pass2InputRef = useRef<HTMLInputElement | null>(null);
+  const genderInputRef = useRef<HTMLInputElement | null>(null);
+  const acceptInputRef = useRef<HTMLInputElement | null>(null);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -19,7 +25,69 @@ export default function UncontrolledForm() {
           fieldId="name"
           placeholderText="Enter your name"
         />
-
+        <CustomInput
+          fieldName="Age"
+          ref={ageInputRef}
+          type="number"
+          fieldId="lastname"
+          placeholderText="Enter your last name"
+        />
+        <CustomInput
+          fieldName="Enter Email"
+          ref={emailInputRef}
+          type="text"
+          fieldId="email"
+          placeholderText="Enter email"
+        />
+        <CustomInput
+          fieldName="Password"
+          ref={pass1InputRef}
+          type="password"
+          fieldId="password"
+          placeholderText="Enter Password"
+        />
+        <CustomInput
+          fieldName="Password(second enter)"
+          ref={pass2InputRef}
+          type="password"
+          fieldId="password2"
+          placeholderText="Enter the same Password second time"
+        />
+        <div>
+          <CustomInput
+            fieldName="Gender"
+            ref={genderInputRef}
+            fieldLabel="Male"
+            type="radio"
+            fieldId="gender"
+            placeholderText="Enter the same Password second time"
+          />
+          <CustomInput
+            fieldName="Gender"
+            ref={genderInputRef}
+            fieldLabel="Female"
+            type="radio"
+            fieldId="gender"
+            placeholderText="Enter the same Password second time"
+          />
+          <CustomInput
+            fieldName="Gender"
+            ref={genderInputRef}
+            fieldLabel="Other"
+            type="radio"
+            fieldId="gender"
+            placeholderText="Enter the same Password second time"
+          />
+        </div>
+        <CustomInput
+          fieldName="accept Terms and Conditions agreement"
+          ref={acceptInputRef}
+          type="checkbox"
+          fieldId="accept"
+          placeholderText="Enter the same Password second time"
+        />
+        <div>Picture</div>
+        <div>Country</div>
         <button>Reset</button>
         <button type="submit">Submit</button>
       </div>
